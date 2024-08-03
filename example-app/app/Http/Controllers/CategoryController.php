5d144d7b8e433b42f028baf0100679bae70aafb6
mariata06 <slinkywizard@mail.ru>
2024-08-03 12:08:47 +0300

    Showing all the categories in the table, displaying them by creating date, add serial number and time format

--- example-app/app/Http/Controllers/CategoryController.php
+++ example-app/app/Http/Controllers/CategoryController.php
@@ -11,7 +11,10 @@ use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
     public function AllCat() {
-        return view('admin.category.index');
+        // $categories = category::all();
+        $categories = Category::latest()->get();
+        //для отображения наших данных из базы в таблицу надо передать эти данные (admin.category.index)
+        return view('admin.category.index', compact('categories'));
     }
 
     public function AddCat(Request $request) {
@@ -24,11 +27,12 @@ class CategoryController extends Controller
             'category_name.max' => 'Category Less Then 255Charts',
         ]);
 
-        // Category::insert([
-        //     'category_name' => $request->category_name,
-        //     'user_id' => Auth::user()->id,
-        //     'created_at' => Carbon::now()
-        // ]);
+        // Inserting data then using ORM
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
+        ]);
 
         // $category = new Category;
         // $category -> category_name = $request->category_name;
@@ -36,10 +40,10 @@ class CategoryController extends Controller
         // $category->save();
 
         //Inserting data with Query Builder
-        $data = array();
-        $data['category_name'] = $request->category_name;
-        $data['user_id'] = Auth::user()->id;
-        DB::table('categories')->insert($data);
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id'] = Auth::user()->id;
+        // DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success', 'Category Inserted Successfull');
     }
