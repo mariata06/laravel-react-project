d7097a2e42a8548312b1ec21aa6c859ee79bbe53
mariata06 <slinkywizard@mail.ru>
2024-08-03 11:26:56 +0300

    Inserting new categories by Query Builder

--- example-app/app/Http/Controllers/CategoryController.php
+++ example-app/app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use Auth;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -23,16 +24,22 @@ class CategoryController extends Controller
             'category_name.max' => 'Category Less Then 255Charts',
         ]);
 
-        Category::insert([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-            'created_at' => Carbon::now()
-        ]);
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        //     'created_at' => Carbon::now()
+        // ]);
 
-        $category = new Category;
-        $category -> category_name = $request->category_name;
-        $category -> user_id = Auth::user()->id;
-        $category->save();
+        // $category = new Category;
+        // $category -> category_name = $request->category_name;
+        // $category -> user_id = Auth::user()->id;
+        // $category->save();
+
+        //Inserting data with Query Builder
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success', 'Category Inserted Successfull');
     }
